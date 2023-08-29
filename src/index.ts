7c978856bd1cf28f98f24a7d41cd48ba1db6e796
alirezaheibati <alirezaheibati91@gmail.com>
2023-08-29 09:38:38 +0330

    create todo class

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,20 @@ const description = <HTMLInputElement>(
 const todoForm = document.getElementById("todo-form");
 const unDoneConatiner = document.getElementById("undone-todos_container");
 const doneConatiner = document.getElementById("todos-list_container");
+
+class Todo {
+  id: string;
+  title: string;
+  description: string;
+  category: "done" | "undone";
+  timeStamp: number[];
+  constructor(title: string, description: string, time: number) {
+    this.id = String(Math.random());
+    this.title = title;
+    this.description = description;
+    this.category = "undone";
+    this.timeStamp = [time];
+  }
+}
+
+let todosList: Todo[] = [];
